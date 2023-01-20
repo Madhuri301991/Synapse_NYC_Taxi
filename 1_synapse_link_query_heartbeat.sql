616a4a30d10e6d76b1ab928c145c9d3dad9bd003
Madhuri301991 <83977380+Madhuri301991@users.noreply.github.com>
2023-01-20 21:37:23 +0000

    Update 1_synapse_link_query_heartbeat.sql

--- 1_synapse_link_query_heartbeat.sql
+++ 1_synapse_link_query_heartbeat.sql
@@ -1,6 +1,6 @@
 IF (NOT EXISTS(SELECT * FROM sys.credentials WHERE name = 'cosmos-db-synapse-course'))
     CREATE CREDENTIAL [cosmos-db-synapse-course]
-    WITH IDENTITY = 'SHARED ACCESS SIGNATURE', SECRET = '9oHsY1hH5haetUtXVtCFmrwqGFQQ97Xjq4wfJtCUDi6AVait9TD9GqsC4aajeNXtpw4t9Fp2xvUEACDb6LVxrg=='
+    WITH IDENTITY = 'SHARED ACCESS SIGNATURE', SECRET = ' '
 GO
 
 SELECT TOP 100 *
