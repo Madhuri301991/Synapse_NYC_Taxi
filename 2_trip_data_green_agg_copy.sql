2611b2ed96ce4779d666bae526645557c7a69bd8
Madhuri301991 <83977380+Madhuri301991@users.noreply.github.com>
2023-01-20 14:16:00 +0000

    Final commit

---
+++ 2_trip_data_green_agg_copy.sql
@@ -0,0 +1,15 @@
+-- copy the data from table in datalake container to database in dedicated sql pool using copy statement
+
+COPY INTO dwh.trip_data_green_agg_copy  
+FROM 'https://coursesynapsedatalake.dfs.core.windows.net/nyc-taxi-data/gold/trip_data_green_agg'
+WITH
+(
+	FILE_TYPE = 'PARQUET'
+	,MAXERRORS = 0
+	,COMPRESSION = 'snappy'
+	,AUTO_CREATE_TABLE='ON'
+)
+GO
+
+SELECT TOP 100 * FROM dwh.trip_data_green_agg_copy  
+GO
